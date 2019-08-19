eae65ac0ca0b1dc2021ebd1ebfac254e4aeca2f6
Philippe Perret <philippe.perret@yahoo.fr>
2019-08-19 18:08:44 +0300

    Poursuite test accueil et identification

--- spec/features/home_page/signed_user_spec.rb
+++ spec/features/home_page/signed_user_spec.rb
@@ -1,4 +1,4 @@
-feature "La page d'accueil" do
+feature "La page d'accueil", current: true do
   context 'pour un icarien identifié' do
     before(:each) do
       # TODO Il faut s'assurer que Benoit ait réglé ses options pour
@@ -6,14 +6,15 @@ feature "La page d'accueil" do
       visit('/')
       identify_benoit
     end
-    scenario 'contient des liens valides pour rejoindre le profil, le bureau', current: true do
+    scenario 'contient des liens valides pour rejoindre le profil, le bureau' do
       {
         "VOTRE PROFIL" => ["Votre profil"],
         "VOTRE BUREAU" => ["Votre bureau"]
       }.each do |link_name, title_page|
         expect(page).to have_link(link_name)
         click_link(link_name)
-        expect(page).to have_css('h1', text: title_page)
+        expect(page).to have_css('h1', text: title_page.first)
+        click_link("Atelier Icare")
       end
 
     end
